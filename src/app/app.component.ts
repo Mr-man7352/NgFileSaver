5628181604a3e1519b052e7c081fd53faf372bc0
Manish Sahu <manissh7352@gmail.com>
2022-09-08 18:15:17 +0530

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { saveAs } from 'file-saver';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'fileSaver';
+
+  constructor(private http: HttpClient) {}
+
+  // downlaodfile() {
+  //   saveAs(
+  //     '	https://backendbirma.s3.ap-south-1.amazonaws.com/birmaaKN7k9FAwwOfi6FfqCI%3D.jpeg',
+  //     'image.jpg'
+  //   );
+  // }
+
+  downlaodfile() {
+    this.http
+      .get(
+        'https://backendbirma.s3.ap-south-1.amazonaws.com/birmaaKN7k9FAwwOfi6FfqCI%3D.jpeg',
+        { responseType: 'blob' }
+      )
+      .subscribe((res: any) => {
+        console.log(res);
+
+        saveAs(res, 'file');
+      });
+  }
 }
